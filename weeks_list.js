fe858e4208177a99ded0ddab79d5205cfa243e9c
Justin Williams <justinawcs@gmail.com>
2025-04-14 14:44:06 -0400

    Add dark mode and more visual improvements

--- weeks_list.js
+++ weeks_list.js
@@ -1,4 +1,5 @@
 var curr_year = JSJoda.LocalDate.now()._year;
+var light = true;
 
 function getWeekNumber(date=null){
     if(date == null){
@@ -53,10 +54,11 @@ function makeTable(year=null){
     for(i=0;i<55;i++){
         month = JSJoda.Month.from(date)._name;
         month = month.charAt(0) + month.slice(1).toLowerCase();
-        var str = "<tr><td>" +  month +" "+ date._day +" "+ date._year +
-                  "</td><td class='text-center fw-bold'>" + getWeekNumber(date.toString()) +
-                  "</td><td>" + getWeekLetter(date.toString()) +
-                  "</td></tr>\n";
+        var str =
+                "<tr><td class='text-center'>" + getWeekNumber(date.toString()) +
+                "</td><td>" +  month +" "+ date._day +" "+ date._year +
+                "</td><td class='fw-bold'>" + getWeekLetter(date.toString()) +
+                "</td></tr>\n";
         result += str;
         date = date.plusWeeks(1);
     }
@@ -78,15 +80,40 @@ function changeTable(action){
 }
 
 function fillData(){
-    d = new Date()
+    var d = new JSJoda.LocalDate.now()
+    var month = JSJoda.Month.from(d)._name;
+    month = month.charAt(0) + month.slice(1).toLowerCase();
+    var wk_day = d.dayOfWeek()._name;
+    wk_day = wk_day.charAt(0) + wk_day.slice(1).toLowerCase();
+    var full_date = wk_day+" "+month+" "+d._day+", "+d._year;
+
     document.getElementById("wk_number").innerHTML = getWeekNumber();
     document.getElementById("wk_letter").innerHTML = getWeekLetter();
     document.getElementById("label_a").innerHTML = getExampleCaseNames()[0];
     document.getElementById("label_b").innerHTML = getExampleCaseNames()[1];
     document.getElementById("year").innerHTML = curr_year;
+    document.getElementById("date_today").innerHTML = full_date;
     document.getElementById("wk_table").innerHTML = makeTable();
 }
 
+function lights(){
+    if( light ){
+        light = false;
+        document.documentElement.setAttribute("data-bs-theme", "dark");
+        document.getElementById("page").classList.replace("bg-white", "bg-dark");
+        document.getElementById("more_info").classList.replace("bg-light", "bg-secondary");
+        document.getElementById("switcha").setAttribute("hidden", "")
+        document.getElementById("switchb").removeAttribute("hidden")
+
+    }else{
+        light = true;
+        document.documentElement.setAttribute("data-bs-theme", "");
+        document.getElementById("page").classList.replace("bg-dark", "bg-white");
+        document.getElementById("more_info").classList.replace("bg-secondary","bg-light");
+        document.getElementById("switcha").removeAttribute("hidden")
+        document.getElementById("switchb").setAttribute("hidden", "true")
+    }
+}
 window.onload = function(){
     fillData();
 }
